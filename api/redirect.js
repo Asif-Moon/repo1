2928b9c4ff3ed8690dbdcd149aa462415582ddb1
Asif-Moon <asifaslam.ch94@gmail.com>
2025-01-10 18:33:26 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.ewm.co.uk/2152015";
+    const whitePageURL = "https://www.freepeople.com/shop/maxamillion-charm-necklace/?category=accessories&color=409&type=REGULAR&size=One+Size&quantity=1";
     const blackPageURL = "https://adffuaorrd.myfunnelish.com/women-pink-socks-1736427651655748";
   
     // Parse the UTM parameters from the request URL
